5d1a2aa79f004b4ce0234e28c2acbd9ed9813308
lichen <lichen@leinao.ai>
2020-07-21 12:49:54 +0800

    Initial commit

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export ANDROID_NDK=/home/cli/code/android-openssl/android-ndk-r21
+export ANDROID_NDK=/home/lichen/tools/android-openssl/android-ndk-r21b
 rm -rf build
 mkdir build && cd build
 cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
@@ -9,5 +9,5 @@ cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
 		-DANDROID_ARM_NEON=TRUE  \
 		-DANDROID_STL=c++_shared \
 		..
-make -j8 && make install
+make -j32 && make install
 cd ..
